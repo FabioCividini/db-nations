641623f317e6d06e175a47dcd099687c2122b106
FabioCividini <82501787+FabioCividini@users.noreply.github.com>
2021-12-20 16:20:48 +0100

    Bonus esercizio finito
    Bonus esercizio finito

--- Java JDBC DB Nations/src/main/java/org/generation/italy/Main.java
+++ Java JDBC DB Nations/src/main/java/org/generation/italy/Main.java
@@ -15,6 +15,8 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		boolean valido = false;
+		String tabella = "";
 		
 		try(Connection con = DriverManager.getConnection(URL, USER, PASSWORD)){
 			
@@ -31,14 +33,69 @@ public class Main {
 				
 				try(ResultSet rs = ps.executeQuery()){
 					
-					System.out.format("%5s%50s%30s%30s","ID:", "NAME COUNTRIES:", "REGION:", "CONTINENT:\r");
+					tabella = String.format("%5s%50s%30s%30s","ID:", "NAME COUNTRY:", "REGION:", "CONTINENT:\r");
 					while(rs.next()) {
-						
-						System.out.format("%5s%50s%30s%30s",rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4) + "\r");
+						valido = true;
+						tabella += String.format("%5s%50s%30s%30s",rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4));
 					}
 				}
 				
 			}
+			if (valido) {
+				System.out.println(tabella);
+				System.out.print("Choose a country ID: ");
+				String userId = scan.nextLine();
+				
+				String sql2 = "select c.name from countries c where c.country_id = ?;";
+				try(PreparedStatement ps2 = con.prepareStatement(sql2)){
+					ps2.setString(1, userId);
+					
+					try(ResultSet rs2 = ps2.executeQuery()){
+						
+						while(rs2.next()) {
+							System.out.println("\nDetails for country: " + rs2.getString(1));
+						}
+					}
+				}
+				
+				String sql3 = "select l.`language` from country_languages cl \r\n"
+						+ "join languages l on l.language_id = cl.language_id\r\n"
+						+ "where cl.country_id = ?;";
+				try(PreparedStatement ps3 = con.prepareStatement(sql3)){
+					ps3.setString(1, userId);
+					
+					try(ResultSet rs3 = ps3.executeQuery()){
+						
+						System.out.print("Languages: ");
+						while(rs3.next()) {
+							System.out.print(rs3.getString(1) + "  ");
+						}
+					}
+				}
+				
+				String sql4 = "select `year`, population, gdp \r\n"
+						+ "from country_stats cs \r\n"
+						+ "where cs.country_id = ?\r\n"
+						+ "order by `year` desc\r\n"
+						+ "limit 1;";
+				try(PreparedStatement ps4 = con.prepareStatement(sql4)){
+					ps4.setString(1, userId);
+					
+					try(ResultSet rs4 = ps4.executeQuery()){
+						
+						System.out.println("\nMost recent stats");
+						while(rs4.next()) {
+							System.out.println("Year: " + rs4.getString(1));
+							System.out.println("Population: " + rs4.getString(2));
+							System.out.println("Gdp: " + rs4.getString(3));
+						}
+					}
+				}
+				
+			} else {
+				System.out.println("Non ci sono risultati");
+			}
+			
 			
 		} catch(SQLException se) {
 			System.out.println("ERRORE: " + se.getMessage());
