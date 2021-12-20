39d6f3968ba7471ded67efe5b2749ad5a50c9bb1
FabioCividini <82501787+FabioCividini@users.noreply.github.com>
2021-12-20 12:37:47 +0100

    MILESTONE 3 FINITA

--- Java JDBC DB Nations/src/main/java/org/generation/italy/Main.java
+++ Java JDBC DB Nations/src/main/java/org/generation/italy/Main.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
 	
@@ -13,22 +14,27 @@ public class Main {
 	private final static String PASSWORD = "rootpassword";
 	
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		
 		try(Connection con = DriverManager.getConnection(URL, USER, PASSWORD)){
 			
-			String sql = "select c.name as name_countries, c.country_id as ID_countries, r.name as name_region, c2.name as name_continent from countries c \r\n"
+			System.out.print("Search: ");
+			String ricerca = scan.nextLine();
+			
+			String sql = "select c.country_id as ID_countries, c.name, r.name as name_region, c2.name as name_continent from countries c \r\n"
 					+ "join regions r on c.region_id = r.region_id\r\n"
 					+ "join continents c2 on c2.continent_id = r.continent_id\r\n"
+					+ "where c.name like ?\r\n"
 					+ "order by c.name;";
 			try(PreparedStatement ps = con.prepareStatement(sql)){
+				ps.setString(1, "%" + ricerca + "%");
 				
 				try(ResultSet rs = ps.executeQuery()){
 					
+					System.out.format("%5s%50s%30s%30s","ID:", "NAME COUNTRIES:", "REGION:", "CONTINENT:\r");
 					while(rs.next()) {
-						System.out.print(rs.getString(1) + " - ");
-						System.out.print(rs.getInt(2) + " - ");
-						System.out.print(rs.getString(3) + " - ");
-						System.out.println(rs.getString(4));
+						
+						System.out.format("%5s%50s%30s%30s",rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4) + "\r");
 					}
 				}
 				
@@ -37,7 +43,8 @@ public class Main {
 		} catch(SQLException se) {
 			System.out.println("ERRORE: " + se.getMessage());
 		}
-
+		
+		scan.close();
 	}
 
 }
